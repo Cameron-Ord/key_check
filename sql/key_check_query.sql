d6420521fac521dcde2c3235da19b84ab3eaa4cd
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-13 14:06:10 -0600

    adding keycheck

---
+++ sql/key_check_query.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie (id, title, runtime, director, release_year) VALUES(0349, 'movie1', 1.5, 'tim', 1990);
+
+INSERT INTO movie (id, title, runtime, director, release_year) VALUES(0215, 'movie2', 1.2, 'jim', 1973);
+
+INSERT INTO movie (id, title, runtime, director, release_year) VALUES(0653, 'movie3', 1.3, 'al', 1980);
